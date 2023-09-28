42274a6ea416a28bdf8c1e5819cde1f9909459c6
Dantallope <dantebraccialini@gmail.com>
2023-09-28 14:43:12 -0600

    fixedEvenOddThing

--- IfElse/main.py
+++ IfElse/main.py
@@ -1,7 +1,15 @@
-# Checks if a number is even or odd
+# If  is odd, print Weird
+# If  is even and in the inclusive range of 2 to 5, print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20, print Not Weird
+
 n = int(input().strip())
 
-if n % 2 == 0:
-    print('Not Weird')
-else:
+if n % 2 != 0:
     print('Weird')
+elif 2 <= n <= 5:
+    print('Not Weird')
+elif 6 <= n <= 20:
+    print('Weird')
+else:
+    print('Not Weird')
